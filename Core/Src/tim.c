40f35aeb536a6e1bfbcf45a7361c133d1be20f38
pedrokappaa <a91998@alunos.uminho.pt>
2022-04-18 23:28:22 +0100

    v3.0.1

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -202,7 +202,7 @@ void MX_TIM2_Init(void)
   htim2.Instance = TIM2;
   htim2.Init.Prescaler = 96-1;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 100-1;
+  htim2.Init.Period = 5000-1;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
